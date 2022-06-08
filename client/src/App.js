4b38e0a8a0ed0e1011a4e2f2f0aeea022c9ad638
Troy Richards <94700828+AyeTone@users.noreply.github.com>
2022-06-08 12:02:06 -0400

    Removed Context items from App

--- client/src/App.js
+++ client/src/App.js
@@ -2,37 +2,20 @@ import { useState } from "react";
 import Comments from "./components/Comments";
 import DeleteModal from "./components/DeleteModal";
 import InputBox from "./components/InputBox";
+import { useContext } from "react";
 import DataContext from "./Context/DataContent";
-import Data from "./data.json";
-import { addComment } from "./components/functions/addComment";
 
 function App() {
-  const [comments, setComments] = useState(Data.comments);
-  const [currentUser, setCurrentUser] = useState(Data.currentUser);
-  const [displayModal, setDisplayModal] = useState({
-    isDisplayed: false,
-    id: null,
-  });
+  const { displayModal } = useContext(DataContext);
 
   return (
-    <DataContext.Provider
-      value={{
-        comments,
-        setComments,
-        currentUser,
-        setDisplayModal,
-        displayModal,
-        addComment,
-      }}
-    >
-      <div className="App">
-        {displayModal.isDisplayed && <DeleteModal />}
-        <main className="content">
-          <Comments />
-          <InputBox />
-        </main>
-      </div>
-    </DataContext.Provider>
+    <div className="App">
+      {displayModal.isDisplayed && <DeleteModal />}
+      <main className="content">
+        <Comments />
+        <InputBox />
+      </main>
+    </div>
   );
 }
 
--- client/src/Context/DataContent.jsx
+++ client/src/Context/DataContent.jsx
@@ -1,5 +1,26 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+import Data from "../data.json";
 
 const DataContext = createContext();
+
+export const DataProvider = ({ children }) => {
+  const [comments, setComments] = useState(Data.comments);
+  const [currentUser, setCurrentUser] = useState(Data.currentUser);
+  const [displayModal, setDisplayModal] = useState({
+    isDisplayed: false,
+    id: null,
+  });
+
+  const value = {
+    comments,
+    setComments,
+    currentUser,
+    setCurrentUser,
+    displayModal,
+    setDisplayModal,
+  };
+
+  return <DataContext.Provider value={value}>{children}</DataContext.Provider>;
+};
 
 export default DataContext;
